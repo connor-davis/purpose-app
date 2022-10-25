870fd031a7a7a878bf57f4dc4b8f5345cc3050d0
Connor Davis <cnnrproton@gmail.com>
2022-10-25 08:41:11 +0200

    Fix api url to match production when releasing a production build.

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,3 +1,3 @@
-// export default 'https://purposeapi.lone-wolf.software/api/v1';
+export default 'https://purposeapi.lone-wolf.software/api/v1';
 // export default 'http://192.168.101.120:4000/api/v1';
-export default 'http://localhost/api/v1';
+// export default 'http://localhost/api/v1';
